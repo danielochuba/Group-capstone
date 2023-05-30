cf5c8fbeb38cde965093d4d0b845c694dddd9f5a
Gardimy Charles <gardimycharles889@gmail.com>
2023-05-30 12:59:42 -0400

    Update popup.js file

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import './style.css';
 import homePage from './modules/home.js';
+import getserie from './modules/popup.js';
 
 homePage();
+getserie();
--- src/modules/popup.js
+++ src/modules/popup.js
@@ -1,42 +1,52 @@
-const popupContainer = document.querySelector('.popupContainer');
+const showPopup = () => {
+  const popupContainer = document.querySelector('.popupContainer');
+  const urlAPI = 'https://api.tvmaze.com/shows/1/seasons';
 
-const urlAPI = 'https://api.tvmaze.com/shows/1/seasons';
+  fetch(urlAPI)
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(serie => {
+        const pop = document.createElement('div');
+        pop.className = 'popup-container';
+        pop.innerHTML = `
+          <div class="seriePop">
+            <button class='closepopup'>&times;</button>    
+            <div class="seriePop-head">
+              <h2>${serie.name}</h2> 
+              <img class="serie-img" src="${serie.image.medium}" alt="serie image">        
+              <ul class="serieInfo">
+                <li>Language: ${serie.language}</li>
+                <li>Premiered: ${serie.premiered}</li>
+                <li>Summary: ${serie.summary}</li>           
+              </ul>
+            </div>
+            <div class="formPop">
+              <h2>Add a Comment</h2>
+              <form>
+                <input class='username' 
+                  type="text" 
+                  name="username" 
+                  placeholder="Your Name" required>
+                <textarea class='comment' 
+                  id="Comment" placeholder="Your insights" 
+                  maxlength="600" 
+                  name="message" required></textarea>
+                <button class="commentButton" data-index="${serie.id}">comment</button>
+              </form>
+            </div>      
+          </div>`;
 
-const createPopup = (serie) => {
-  const pop = document.createElement('div');
-  pop.className = 'popup-container';
-  pop.innerHTML = `
-      <div class="seriePop">
-         <button class='closepopup'>&times;</button>    
-        <div class="seriePop-head">
-          <h2>${serie.name}</h2> 
-          <img class="serie-img" src="${serie.img}" alt="serie image">        
-          <ul class="serieInfo">
-            <li>name1 : ${serie.name1}</li>
-            <li>seasons : ${serie.season} </li>
-            <li>episode : ${serie.episode}</li>           
-          </ul>
-        </div>
-        <div class="formPop">
-          <h2>Add a Comment</h2>
-          <form>
-            <input class='username' 
-            type="text" 
-            name="username" 
-            placeholder="Your 
-            Name" required>
-            <textarea class='comment' 
-            id="Comment" placeholder="Your comments" 
-            maxlength="600" 
-            name="message" required></textarea>
-            <button class="commentButton" data-index="${serie.id}">Go</button>
-          </form>
-        </div>      
-      </div>`;
-
-  popupContainer.appendChild(pop);
-  const close = document.querySelector('.closepopup');
-  close.addEventListener('click', () => {
-    popupContainer.removeChild(pop);
-  });
+        popupContainer.appendChild(pop);
+        const close = pop.querySelector('.closepopup');
+        close.addEventListener('click', () => {
+          popupContainer.removeChild(pop);
+        });
+      });
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
 };
+
+// Call the showPopup function to fetch data and display popups
+showPopup();
