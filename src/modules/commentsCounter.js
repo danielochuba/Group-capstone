0d4699c0ae51ec63737499012126144c1cb31b21
Daniel Ochuba <danielochuba78@gmail.com>
2023-06-03 22:53:58 +0100

    Make requested changes

--- src/modules/commentsCounter.js
+++ src/modules/commentsCounter.js
@@ -1,8 +1,5 @@
-// eslint-disable-next-line import/no-cycle
-import { getSpecificComment } from './commentsApiConfig.js';
-
-const countComments = async (id) => {
-  const commentsData = await getSpecificComment(id);
+const countComments = async (func, id) => {
+  const commentsData = await func(id);
   const commetLabel = document.querySelector('.comment-count');
   commetLabel.innerHTML = commentsData.length.toString();
 };
