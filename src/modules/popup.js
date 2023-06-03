3314651c876fef3f2254d93a23820e3283b83674
Gardimy Charles <gardimycharles889@gmail.com>
2023-06-02 21:36:31 -0400

    fix error

--- src/modules/popup.js
+++ src/modules/popup.js
@@ -8,7 +8,6 @@ const showPopup = async () => {
   const endDate = document.querySelector('.end-date');
   const movieLink = document.querySelector('.movie-link');
   const summaryText = document.querySelector('.summary-text');
-  let commentSumitBtn;
 
   const commentBtns = document.querySelectorAll('.comments-btn');
   commentBtns.forEach((btn, index) => {
