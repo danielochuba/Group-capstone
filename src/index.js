e3614ef1de69ea18c8773c3a143eec0ecd8a87b1
Daniel Ochuba <danielochuba78@gmail.com>
2023-06-03 13:16:04 +0100

    Add items Counter Feature

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import './style.css';
 import homePage from './modules/home.js';
 import showPopup from './modules/popup.js';
+import countItems from './modules/itemsCounter.js';
 
-homePage();
+const app = async () => {
+  await homePage();
+  countItems();
+};
+
+app();
 
 window.document.addEventListener('DOMContentLoaded', () => {
   showPopup();
